481e8c559a893b42b031b74d02d75eb601b4e34a
Aleksey Rybalkin <aleksey@rybalkin.org>
2020-05-16 16:13:20 +0300

    python-addons updates

--- addons/youtube.py
+++ addons/youtube.py
@@ -10,9 +10,7 @@ import addons.db
 channels = [
     'UCDYZxJE8kLZ-o6nL8E1bXdQ',  # MATN
     'UCKab3hYnOoTZZbEUQBMx-ww',  # NerdCubed
-    'UCNUfNaego-snWXd8vygfpAg',  # sprEEEzy
     'UC7-E5xhZBZdW-8d7V80mzfg',  # JennyENicholson
-    'UC-lHJZR3Gqxm24_Vd_AJ5Yw',  # PewDiePie
 ]
 
 
