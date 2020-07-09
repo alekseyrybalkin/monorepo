4cd93aa8db7f6e85c21413f41285b9a271d02080
Aleksey Rybalkin <aleksey@rybalkin.org>
2020-07-09 17:28:21 +0300

    Revert "python-addons updates"
    This reverts commit 2f3ea059bf5ff4b0af49ea1dd6cf34b1ea7ae078.

--- addons/youtube.py
+++ addons/youtube.py
@@ -10,13 +10,7 @@ import addons.db
 channels = [
     'UCDYZxJE8kLZ-o6nL8E1bXdQ',  # MATN
     'UCKab3hYnOoTZZbEUQBMx-ww',  # NerdCubed
-    'UCNUfNaego-snWXd8vygfpAg',  # sprEEEzy
     'UC7-E5xhZBZdW-8d7V80mzfg',  # JennyENicholson
-    'UC-lHJZR3Gqxm24_Vd_AJ5Yw',  # PewDiePie
-    'UCOpcACMWblDls9Z6GERVi1A',  # Screen Junkies
-    'UC1cVTv6C0Unv6TtadjBvYRA',  # Rapid Trailer
-    'UCf6J9yokPS0ys456jvjLBGQ',  # Fandom Games
-    'UCbiOAho0h23IMInURiESx1w',  # Dan Murrell
 ]
 
 
