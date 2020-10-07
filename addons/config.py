4ecff6b449e79c3289c205bc68cd4e6799333908
Aleksey Rybalkin <aleksey@rybalkin.org>
2020-10-07 21:00:20 +0300

    python-addons updates

--- addons/config.py
+++ addons/config.py
@@ -5,9 +5,10 @@ import addons.shell as shell
 
 
 class Config:
-    def __init__(self, name, private=True):
+    def __init__(self, name, private=True, defaults=None):
         self.name = name
         self.private = private
+        self.defaults = defaults
 
     def read(self):
         if not self.private:
@@ -23,6 +24,20 @@ class Config:
                 'private',
                 '{}.json'.format(self.name),
             )
+
+        os.makedirs(os.path.dirname(conffile_path), exist_ok=True)
+
+        if not os.path.exists(conffile_path):
+            if defaults is None:
+                raise ValueError('Config file does not exist and no defaults were provided')
+            with open(conffile_path, 'tw') as conffile:
+                if isinstance(defaults, str):
+                    conffile.write(defaults)
+                elif isinstance(defaults, dict):
+                    conffile.write(json.dumps(defaults))
+                else:
+                    raise ValueError('Wrong default config format')
+
         with open(conffile_path, 'tr') as conffile:
             config = json.loads(conffile.read())
         return config
