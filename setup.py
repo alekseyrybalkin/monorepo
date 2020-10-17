4cfe44ef95cf39dc43d7d4cbcd09570ca5a3f1c4
Aleksey Rybalkin <aleksey@rybalkin.org>
2020-10-17 23:46:18 +0300

    python-addons updates

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     entry_points={
         'console_scripts': [
             'compress-music = addons.util.compress_music:compress',
-            'gen = addons.util.nmap:main',
+            'nmap = addons.util.nmap:main',
             'gen = addons.util.gen:gen',
             'github-2fa = addons.util.github:genpass',
             'srcfetcher = addons.srcfetcher:main',
