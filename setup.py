ba47896c883467545d5d05c7825909bfea7e829f
Aleksey Rybalkin <aleksey@rybalkin.org>
2020-10-27 14:41:19 +0300

    python-addons updates

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(
             'compress-music = addons.util.compress_music:compress',
             'nmap = addons.util.nmap:main',
             'gen = addons.util.gen:gen',
-            'github-2fa = addons.util.github:genpass',
             'srcfetcher = addons.srcfetcher:main',
             'take-screenshot = addons.util.screenshot:main',
             'hckrnews = addons.hckrnews.hckrnews:main',
