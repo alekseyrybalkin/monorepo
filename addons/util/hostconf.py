017f838a1124f6807de25ae5a8821e0107eae9dd
Aleksey Rybalkin <aleksey@rybalkin.org>
2020-11-09 00:22:58 +0300

    python-addons updates

--- addons/util/hostconf.py
+++ addons/util/hostconf.py
@@ -6,7 +6,7 @@ import addons.config
 class HostConf:
     def parse_args(self):
         parser = argparse.ArgumentParser()
-        parser.add_argument('option', type=str)
+        parser.add_argument('option', type=str, default='label', nargs='?')
         return parser.parse_args()
 
     def main(self):
@@ -16,7 +16,10 @@ class HostConf:
         with open(config['label-file'], 'tr') as label_file:
             label = label_file.read().strip()
 
-        print(config['hosts'][label][args.option])
+        if args.option == 'label':
+            print(label)
+        else:
+            print(config['hosts'][label][args.option])
 
 
 def main():
