acf041265cdd5d4ec905214587f69afe47c9ca06
Aleksey Rybalkin <aleksey@rybalkin.org>
2020-11-13 12:24:17 +0300

    python-addons updates

--- addons/ji/make.py
+++ addons/ji/make.py
@@ -1,8 +1,28 @@
+import os
+
 import addons.ji.common as common
+import addons.shell as shell
 
 
 def prepare(pm):
+    shell.run('function prepare() { :; }', shell=True)
     pkgbuild = common.source_pkgbuild(pm)
+
+    if pkgbuild['vcs']:
+        urls = pkgbuild['extra_urls'].split(' ')
+    else:
+        urls = pkgbuild['urls'].split(' ') + pkgbuild['extra_urls'].split(' ')
+    urls.remove('')
+
+    for url in urls:
+        path = os.path.join(pm.config['tarballs_path'], os.path.basename(url))
+        if not os.path.isfile(path):
+            raise RuntimeError('file {} not found'.format(url))
+
+    for item in os.listdir('.'):
+        shell.run('git ls-files {} --error-unmatch'.format(item))
+
+    shell.run('prepare', shell=True)
 
 
 def make(pm):
