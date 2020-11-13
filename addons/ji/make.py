4a01191a11ff270a45dd43c877922255f1044e45
Aleksey Rybalkin <aleksey@rybalkin.org>
2020-11-13 12:34:00 +0300

    python-addons updates

--- addons/ji/make.py
+++ addons/ji/make.py
@@ -22,7 +22,7 @@ def prepare(pm):
     for item in os.listdir('.'):
         shell.run('git ls-files {} --error-unmatch'.format(item))
 
-    shell.run('type prepare >/dev/null 2>&1 || function prepare() { :; }; prepare', shell=True)
+    shell.run('source PKGBUILD; type prepare >/dev/null 2>&1 || function prepare() { :; }; prepare', shell=True)
 
 
 def make(pm):
