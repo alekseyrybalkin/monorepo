5867cf1e4bd012d85135f55cf583796336c77032
Aleksey Rybalkin <aleksey@rybalkin.org>
2020-11-13 13:17:08 +0300

    python-addons updates

--- addons/helpers.py
+++ addons/helpers.py
@@ -8,7 +8,7 @@ import addons.shell as shell
 def get_battery_charges():
     charges = []
     if shutil.which('acpi'):
-        acpi_output = shell.run('acpi')
+        acpi_output = shell.run('acpi', silent=True)
         for line in acpi_output.split('\n'):
             charges.append(int(re.match(r'^.* (\d+)%.*$', line).group(1)))
     return charges
