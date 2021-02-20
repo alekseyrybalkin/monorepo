95886ac55055243a3c479866ae946135b4faf999
Aleksey Rybalkin <aleksey@rybalkin.org>
2021-02-20 17:40:23 +0300

    packmgr: upgrade: fallback to copy+move in case of "Invalid cross-device link" error

--- mr/packmgr/upgrade.py
+++ mr/packmgr/upgrade.py
@@ -50,7 +50,11 @@ def upgrade(pm, tar):
 
         with shell.popd(tmpdir):
             for item in files:
-                os.replace(item.name, os.path.join('/', item.name))
+                try:
+                    os.replace(item.name, os.path.join('/', item.name))
+                except OSError:
+                    shutil.move(item.name, os.path.join('/', item.name + '.{}'.format(pm.config['exe'])))
+                    os.replace(os.path.join('/', item.name + '.{}'.format(pm.config['exe'])), os.path.join('/', item.name))
 
     old_files = set(queries.db_list_files(pm, package['name']))
     new_files = set(os.path.join('/', item.name) for item in tarball.list_files(pm, tar))
